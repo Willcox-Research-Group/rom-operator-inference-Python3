6c88244bf87192c00a860cad0528f7e54476da4f
Shane <shanemcq18@gmail.com>
2019-08-20 15:24:24 -0500

    add more protective checks to pre.py

--- rom_operator_inference/pre.py
+++ rom_operator_inference/pre.py
@@ -2,12 +2,21 @@
 """Tools for pre-processing data """
 
 import numpy as np
-import numba as _numba
 from scipy.linalg import svd as _svd
 from scipy.sparse.linalg import svds as _svds
 from sklearn.utils.extmath import randomized_svd as _rsvd
 
+# Attempt to import numba (not included in the default requirements list).
+try:
+    import numba as _numba
+except ImportError:                                         # pragma: no cover
+    # If numba isn't installed, make an empty decorator in its place.
+    def _numba(): pass
+    def _jit(nopython=True): return lambda x: x
+    _numba.jit = _jit
 
+
+# Basis computation ===========================================================
 def mean_shift(X):
     """Compute the mean of the columns of X, then use it to shift the columns
     so that they have mean zero.
@@ -26,8 +35,12 @@ def mean_shift(X):
     Xshifted : (n,k) ndarray
         The matrix such that Xshifted[:,j] + xbar = X[:,j] for j=1,2,...,k.
     """
+    # Check dimensions.
+    if X.ndim != 2:
+        raise ValueError("data X must be two-dimensional")
+
     xbar = np.mean(X, axis=1)               # Compute the mean column.
-    Xshifted = X - xbar.reshape((-1,1))     # Subtract by the mean.
+    Xshifted = X - xbar.reshape((-1,1))     # Shift the columns by the mean.
     return xbar, Xshifted
 
 
@@ -73,44 +86,45 @@ def pod_basis(X, r, mode="arpack", **options):
     elif mode == "randomized":
         return _rsvd(X, r, **options)[0][:,::-1]
     else:
-        raise ValueError(f"invalid mode '{mode}'")
+        raise NotImplementedError(f"invalid mode '{mode}'")
 
 
+# Derivative approximation ====================================================
 @_numba.jit(nopython=True)
-def _fwd4(Y, dt):                                           # pragma: no cover
-    """Compute the first column-wise derivative of a uniformly-spaced 2D array
-    with a 4th order forward difference scheme.
+def _fwd4(y, dt):                                           # pragma: no cover
+    """Compute the first derivative of a uniformly-spaced-in-time array with a
+    fourth-order forward difference scheme.
 
     Parameters
     ----------
-    Y : (n,5) ndarray
-        The data to differentiate.
+    y : (5,...) ndarray
+        Data to differentiate. The derivative is taken along the first axis.
 
     Returns
     -------
-    dy0 : (n,) ndarray
-        Approximate derivative of Y at the first column, i.e., dY[:,0] / dt.
+    dy0 : float or (...) ndarray
+        Approximate derivative of y at the first entry, i.e., dy[0] / dt.
     """
-    return (-25*Y[:,0] + 48*Y[:,1] - 36*Y[:,2] + 16*Y[:,3] - 3*Y[:,4])/(12*dt)
+    return (-25*y[0] + 48*y[1] - 36*y[2] + 16*y[3] - 3*y[4]) / (12*dt)
 
 
 @_numba.jit(nopython=True)
-def _fwd6(Y, dt):                                           # pragma: no cover
-    """Compute the first column-wise derivative of a uniformly-spaced 2D array
-    with a 6th order forward difference scheme.
+def _fwd6(y, dt):                                           # pragma: no cover
+    """Compute the first derivative of a uniformly-spaced-in-time array with a
+    sixth-order forward difference scheme.
 
     Parameters
     ----------
-    Y : (n,7) ndarray
-        The data to differentiate.
+    y : (7,...) ndarray
+        Data to differentiate. The derivative is taken along the first axis.
 
     Returns
     -------
-    dy0 : (n,) ndarray
-        Approximate derivative of Y at the first column, i.e., dY[:,0] / dt.
+    dy0 : float or (...) ndarray
+        Approximate derivative of y at the first entry, i.e., dy[0] / dt.
     """
-    return (-147*Y[:,0] + 360*Y[:,1] - 450*Y[:,2] + 400*Y[:,3] \
-            - 225*Y[:,4] + 72*Y[:,5] - 10*Y[:,6]) / (60*dt)
+    return (-147*y[0] + 360*y[1] - 450*y[2] + 400*y[3] - 225*y[4] \
+                                              + 72*y[5] - 10*y[6]) / (60*dt)
 
 
 def compute_xdot_uniform(X, dt, order=2):
@@ -121,12 +135,12 @@ def compute_xdot_uniform(X, dt, order=2):
     ----------
     X : (n,k) ndarray
         The data to estimate the derivative of. The jth column is a snapshot
-        that corresponds to the jth time step, i.e., x(t[j]) = X[:,j].
+        that corresponds to the jth time step, i.e., X[:,j] = x(t[j]).
 
     dt : float
         The time step between the snapshots, i.e., t[j+1] - t[j] = dt.
 
-    order : int {2, 4}
+    order : int {2, 4, 6}
         The order of the derivative approximation.
         See https://en.wikipedia.org/wiki/Finite_difference_coefficient.
 
@@ -136,56 +150,58 @@ def compute_xdot_uniform(X, dt, order=2):
         Approximate time derivative of the snapshot data. The jth column is
         the derivative dx / dt corresponding to the jth snapshot, X[:,j].
     """
+    # Check dimensions and input types.
+    if X.ndim != 2:
+        raise ValueError("data X must be two-dimensional")
+    if not np.isscalar(dt):
+        raise TypeError("time step dt must be a scalar (e.g., float)")
+
     if order == 2:
         return np.gradient(X, dt, edge_order=2, axis=1)
 
     Xdot = np.empty_like(X)
+    n,k = X.shape
     if order == 4:
         # Central difference on interior
         Xdot[:,2:-2] = (X[:,:-4] - 8*X[:,1:-3] + 8*X[:,3:-1] - X[:,4:])/(12*dt)
 
         # Forward difference on the front.
-        Xdot[:,0] = _fwd4(X[:,:5], dt)
-        Xdot[:,1] = _fwd4(X[:,1:6], dt)
-
-        # Backward difference on the end.
-        Xdot[:,-1] = -_fwd4(X[:,-5:][:,::-1], dt)
-        Xdot[:,-2] = -_fwd4(X[:,-6:-1][:,::-1], dt)
+        for j in range(2):
+            Xdot[:,j] = _fwd4(X[:,j:j+5].T, dt)                 # Forward
+            Xdot[:,-j-1] = -_fwd4(X[:,-j-5:k-j].T[::-1], dt)    # Backward
 
     elif order == 6:
         # Central difference on interior
         Xdot[:,3:-3] = (-X[:,:-6] + 9*X[:,1:-5] - 45*X[:,2:-4] \
                         + 45*X[:,4:-2] - 9*X[:,5:-1] + X[:,6:]) / (60*dt)
 
-        # Forward difference on the front.
-        Xdot[:,0] = _fwd6(X[:,:7], dt)
-        Xdot[:,1] = _fwd6(X[:,1:8], dt)
-        Xdot[:,2] = _fwd6(X[:,2:9], dt)
-
-        # Backward difference on the end.
-        Xdot[:,-1] = -_fwd6(X[:,-7:][:,::-1], dt)
-        Xdot[:,-2] = -_fwd6(X[:,-8:-1][:,::-1], dt)
-        Xdot[:,-3] = -_fwd6(X[:,-9:-2][:,::-1], dt)
+        # Forward / backward differences on the front / end.
+        for j in range(3):
+            Xdot[:,j] = _fwd6(X[:,j:j+7].T, dt)                 # Forward
+            Xdot[:,-j-1] = -_fwd6(X[:,-j-7:k-j].T[::-1], dt)    # Backward
 
     else:
-        raise ValueError(f"invalid order '{order}'")
+        raise NotImplementedError(f"invalid order '{order}'")
 
     return Xdot
 
-compute_xdot = compute_xdot_uniform
+compute_xdot = compute_xdot_uniform                 # Slightly shorter alias.
+
 
 def compute_xdot_nonuniform(X, t):
-    """Approximate the time derivatives for a chunk of snapshots with second-
-    order finite differences.
+    """Approximate the time derivatives for a chunk of snapshots with a
+    second-order finite difference scheme.
 
     Parameters
     ----------
     X : (n,k) ndarray
         The data to estimate the derivative of. The jth column is a snapshot
-        that corresponds to the jth time step, i.e., x(t[j]) = X[:,j].
+        that corresponds to the jth time step, i.e., X[:,j] = x(t[j]).
 
     t : (k,) ndarray
-        The times corresponding to the snapshots.
+        The times corresponding to the snapshots. May not be uniformly spaced.
+        See compute_xdot_uniform() for higher-order computation in the case of
+        evenly-spaced-in-time snapshots.
 
     Returns
     -------
@@ -193,4 +209,13 @@ def compute_xdot_nonuniform(X, t):
         Approximate time derivative of the snapshot data. The jth column is
         the derivative dx / dt corresponding to the jth snapshot, X[:,j].
     """
-    return np.gradient(X, t, edge_order=2, axis=1)
+    # Check dimensions.
+    if X.ndim != 2:
+        raise ValueError("data X must be two-dimensional")
+    if t.ndim != 1:
+        raise ValueError("time t must be one-dimensional")
+    if X.shape[-1] != t.shape[0]:
+        raise ValueError("data X not aligned with time t")
+
+    # Compute the derivative with a second-order difference scheme.
+    return np.gradient(X, t, edge_order=2, axis=-1)
--- tests/test_pre.py
+++ tests/test_pre.py
@@ -26,6 +26,11 @@ def test_mean_shift(set_up_basis_data):
     assert np.allclose(np.mean(Xshifted, axis=1), np.zeros(X.shape[0]))
     assert np.allclose(xbar.reshape((-1,1)) + Xshifted, X)
 
+    # Try using bad data shape.
+    with pytest.raises(ValueError) as exc:
+        roi.pre.mean_shift(np.random.random((3,3,3)))
+    assert exc.value.args[0] == "data X must be two-dimensional"
+
 
 def test_pod_basis(set_up_basis_data):
     """Test pre.pod_basis() on a small case with the ARPACK solver."""
@@ -34,7 +39,7 @@ def test_pod_basis(set_up_basis_data):
     r = k // 10
 
     # Attempt an invalid mode.
-    with pytest.raises(ValueError) as exc:
+    with pytest.raises(NotImplementedError) as exc:
         roi.pre.pod_basis(X, r, mode="dense")
     assert exc.value.args[0] == "invalid mode 'dense'"
 
@@ -61,9 +66,7 @@ def test_pod_basis(set_up_basis_data):
 # Differentiation routines ----------------------------------------------------
 DynamicState = namedtuple("DynamicState", ["time", "state", "derivative"])
 
-@pytest.fixture
-def set_up_uniform_difference_data():
-    t = np.linspace(0, 1, 400)
+def _difference_data(t):
     Y = np.row_stack((t,
                       t**2/2,
                       t**3/3,
@@ -83,15 +86,36 @@ def set_up_uniform_difference_data():
     return DynamicState(t, Y, dY)
 
 
+@pytest.fixture
+def set_up_uniform_difference_data():
+    t = np.linspace(0, 1, 400)
+    return _difference_data(t)
+
+
+@pytest.fixture
+def set_up_nonuniform_difference_data():
+    t = np.linspace(0, 1, 400)**2
+    return _difference_data(t)
+
+
 def test_fwd4(set_up_uniform_difference_data):
     """Test pre._fwd4()."""
     dynamicstate = set_up_uniform_difference_data
     t, Y, dY = dynamicstate.time, dynamicstate.state, dynamicstate.derivative
     dt = t[1] - t[0]
     for j in range(Y.shape[1] - 5):
-        dYj = roi.pre._fwd4(Y[:,j:j+5], dt)
+        # One-dimensional test.
+        dY0 = roi.pre._fwd4(Y[0,j:j+5], dt)
+        assert isinstance(dY0, float)
+        assert np.isclose(dY0, dY[0,j])
+
+        # Two-dimensional test.
+        dYj = roi.pre._fwd4(Y[:,j:j+5].T, dt)
         assert dYj.shape == Y[:,j].shape
         assert np.allclose(dYj, dY[:,j])
+
+        # Check agreement.
+        assert dY0 == dYj[0]
 
 
 def test_fwd6(set_up_uniform_difference_data):
@@ -100,9 +124,18 @@ def test_fwd6(set_up_uniform_difference_data):
     t, Y, dY = dynamicstate.time, dynamicstate.state, dynamicstate.derivative
     dt = t[1] - t[0]
     for j in range(Y.shape[1] - 7):
-        dYj = roi.pre._fwd6(Y[:,j:j+7], dt)
+        # One-dimensional test.
+        dY0 = roi.pre._fwd6(Y[0,j:j+7], dt)
+        assert isinstance(dY0, float)
+        assert np.isclose(dY0, dY[0,j])
+
+        # Two-dimensional test.
+        dYj = roi.pre._fwd6(Y[:,j:j+7].T, dt).T
         assert dYj.shape == Y[:,j].shape
         assert np.allclose(dYj, dY[:,j])
+
+        # Check agreement.
+        assert dY0 == dYj[0]
 
 
 def test_compute_xdot_uniform(set_up_uniform_difference_data):
@@ -113,33 +146,23 @@ def test_compute_xdot_uniform(set_up_uniform_difference_data):
     for o in [2, 4, 6]:
         dY_ = roi.pre.compute_xdot_uniform(Y, dt, order=o)
         assert dY_.shape == Y.shape
+        print("(order, error):", (o, np.linalg.norm(dY - dY_)))
         assert np.allclose(dY, dY_, atol=1e-4)
 
+    # Try with bad data shape.
     with pytest.raises(ValueError) as exc:
+        roi.pre.compute_xdot_uniform(Y[:,0], dt, order=2)
+    assert exc.value.args[0] == "data X must be two-dimensional"
+
+    # Try with bad order.
+    with pytest.raises(NotImplementedError) as exc:
         roi.pre.compute_xdot_uniform(Y, dt, order=-1)
     assert exc.value.args[0] == "invalid order '-1'"
 
-
-@pytest.fixture
-def set_up_nonuniform_difference_data():
-    t = np.linspace(0, 1, 400)**2
-    Y = np.row_stack((t,
-                      t**2/2,
-                      t**3/3,
-                      np.sin(t),
-                      np.exp(t),
-                      1/(t+1),
-                      t + t**2/2 + t**3/3 + np.sin(t) - np.exp(t)
-                    ))
-    dY = np.row_stack((np.ones_like(t),
-                       t,
-                       t**2,
-                       np.cos(t),
-                       np.exp(t),
-                       -1/(t+1)**2,
-                       1 + t + t**2 + np.cos(t) - np.exp(t)
-                     ))
-    return DynamicState(t, Y, dY)
+    # Try with bad dt type.
+    with pytest.raises(TypeError) as exc:
+        roi.pre.compute_xdot_uniform(Y, np.array([dt, 2*dt]), order=-1)
+    assert exc.value.args[0] == "time step dt must be a scalar (e.g., float)"
 
 
 def test_compute_xdot_nonuniform(set_up_nonuniform_difference_data):
@@ -149,3 +172,17 @@ def test_compute_xdot_nonuniform(set_up_nonuniform_difference_data):
     dY_ = roi.pre.compute_xdot_nonuniform(Y, t)
     assert dY_.shape == Y.shape
     assert np.allclose(dY, dY_, atol=1e-4)
+
+    # Try with bad data shape.
+    with pytest.raises(ValueError) as exc:
+        roi.pre.compute_xdot_nonuniform(Y[:,0], t)
+    assert exc.value.args[0] == "data X must be two-dimensional"
+
+    # Try with bad time shape.
+    with pytest.raises(ValueError) as exc:
+        roi.pre.compute_xdot_nonuniform(Y, np.dstack((t,t)))
+    assert exc.value.args[0] == "time t must be one-dimensional"
+
+    with pytest.raises(ValueError) as exc:
+        roi.pre.compute_xdot_nonuniform(Y, np.hstack((t,t)))
+    assert exc.value.args[0] == "data X not aligned with time t"
