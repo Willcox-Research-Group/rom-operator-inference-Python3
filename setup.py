14e012e4d900d99ed1e1048f43c5bb653eef4549
Shane <shanemcq18@gmail.com>
2020-01-13 14:01:43 -0600

    clip affine inference for now

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as infile:
 setuptools.setup(
     # Package name and version.
     name="rom_operator_inference-shanemcq18",
-    version="0.6.0",
+    version="0.6.1",
 
     # Package description, license, and keywords.
     description="Operator inference for data-driven, non-intrusive model reduction of dynamical systems.",
     license="MIT",
     long_description=readme_text,
     long_description_content_type="text/markdown",
-    url="https://github.com/Willcox-Research-Group/rom-operator-inference-Python3",
+    url="https://github.com/shanemcq18/rom-operator-inference-Python3",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Natural Language :: English",
