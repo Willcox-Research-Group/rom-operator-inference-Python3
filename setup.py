f0e3ed1ac4749baa6c3658cc0acb77abdb469e4c
Shane <shanemcq18@gmail.com>
2021-04-01 17:34:59 -0500

    bump Python requirement to 3.6

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
     # Technical details: source code, dependencies, test suite.
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=[
         "h5py>=2.9.0",
         "numpy>=1.16",
