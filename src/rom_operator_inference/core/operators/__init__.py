dcf65d07cf4ef28b9db4ad63efb8b73441209272
Shane <shanemcq18@gmail.com>
2022-03-14 21:49:14 -0500

    InterpolatedOperator classes (remove Spline1dOperators)

--- src/rom_operator_inference/core/operators/__init__.py
+++ src/rom_operator_inference/core/operators/__init__.py
@@ -9,10 +9,10 @@ G (cubic), and B (input).
 Nonparametric Operator Classes
 ==============================
 These classes represent operators that do not depend on external parameters.
-* ConstantOperator: constant operators (c)
-* LinearOperator: linear operators for state and input (A, B)
-* QuadraticOperator: quadratic operators (H)
-* CubicOperator: quadratic operators (G)
+* ConstantOperator: constant operators, c.
+* LinearOperator: linear operators for state / input, A & B.
+* QuadraticOperator: quadratic operators, H.
+* CubicOperator: quadratic operators, G.
 Used by [Discrete|Continuous]OpInfROM.
 
 The remaining classes are for operators that depend on a external parameters,
@@ -25,10 +25,10 @@ with elementwise interpolation between known operator matrices, i.e.,
     A(µ)[i,j] = Interpolator([µ1, µ2, ...], [A1[i,j], A2[i,j], ...])(µ),
 where µ1, µ2, ... are parameter values and A1, A2, ... are the corresponding
 operator matrices, e.g., A1 = A(µ1).
-* InterpolatedConstantOperator: constant operators c(µ).
-* InterpolatedLinearOperator: linear operators for state and input, A(µ) and B(µ).
-* InterpolatedQuadraticOperator: quadratic operators H(µ).
-* InterpolatedCubicOperator: quadratic operators G(µ).
+* InterpolatedConstantOperator: constant operators, c(µ).
+* InterpolatedLinearOperator: linear operators for state / input, A(µ) & B(µ).
+* InterpolatedQuadraticOperator: quadratic operators, H(µ).
+* InterpolatedCubicOperator: cubic operators, G(µ).
 Used by Interpolated[Discrete|Continuous]OpInfROM.
 
 Affine Operator Classes
@@ -37,10 +37,10 @@ These operators assume the parametric dependence of an operator A = A(µ) has a
 known "affine" structure,
     A(µ) = sum_{i=1}^{nterms} θ_{i}(µ) * A_{i},
 where θ_{i} are scalar-valued functions and A_{i} are matrices.
-* AffineConstantOperator: constant operators c(µ).
-* AffineLinearOperator: linear operators for state and input, A(µ) and B(µ).
-* AffineQuadraticOperator: quadratic operators H(µ).
-* AffineCubicOperator: quadratic operators G(µ).
+* AffineConstantOperator: constant operators, c(µ).
+* AffineLinearOperator: linear operators for state / input, A(µ) & B(µ).
+* AffineQuadraticOperator: quadratic operators, H(µ).
+* AffineCubicOperator: cubic operators, G(µ).
 Used by Affine[Discrete|Continuous]OpInfROM.
 """
 
--- src/rom_operator_inference/core/operators/_interpolate.py
+++ src/rom_operator_inference/core/operators/_interpolate.py
@@ -1,4 +1,4 @@
-# _interpolate.py
+# core/operators/_interpolate.py
 """Classes for parametric operators where the parametric dependence is handled
 with element-wise interpolation, i.e.,
 
@@ -6,15 +6,14 @@ with element-wise interpolation, i.e.,
 """
 
 __all__ = [
-    "Spline1dConstantOperator",
-    "Spline1dLinearOperator",
-    "Spline1dQuadraticOperator",
-    # Spline1dCrossQuadraticOperator",
-    "Spline1dCubicOperator",
+    "InterpolatedConstantOperator",
+    "InterpolatedLinearOperator",
+    "InterpolatedQuadraticOperator",
+    # "InterpolatedCrossQuadraticOperator",
+    "InterpolatedCubicOperator",
 ]
 
 import numpy as np
-import scipy.interpolate
 
 from ._base import _BaseParametricOperator
 from ._nonparametric import (ConstantOperator,
@@ -36,64 +35,67 @@ class _InterpolatedOperator(_BaseParametricOperator):
 
     Attributes
     ----------
-    parameter_values : list of `nterms` scalars or ndarrays
+    parameters : list of `nterms` scalars or ndarrays
         Parameter values at which the operators matrices are known.
     matrices : list of `nterms` ndarray, all of the same shape.
-        Component matrices corresponding to the `parameter_values`.
+        Operator matrices corresponding to the `parameters`, i.e.,
+        `matrices[i]` are the operator entries at the value `parameters[i]`.
+    shape : tuple
+        Shape of the operator entries.
     OperatorClass : class
         Class of operator to construct, a subclass of
         core.operators._BaseNonparametricOperator.
-    InterpolatorClass : scipy.interpolate class
-        Type of interpolator to use. Must obey the following syntax.
-        >>> interpolator_object = Interpolator(data_points, data_values)
-        >>> interpolator_evaluation = interpolator_object(new_data_point)
+    interpolator : scipy.interpolate class (or similar)
+        Object that constructs the operator at new parameter values, i.e.,
+        >>> new_operator_entries = interpolator(new_parameter)
     """
-    # Must be specified by child classes.
-    _InterpolatorClass = NotImplemented
-
-    @property
-    def InterpolatorClass(self):
-        """scipy.interpolate class for the elementwise interpolation."""
-        return self._InterpolatorClass
-
-    def __init__(self, parameter_values, matrices, **kwargs):
-        """Construct the interpolator.
+    def __init__(self, parameters, matrices, InterpolatorClass,
+                 **kwargs):
+        """Construct the elementwise operator interpolator.
 
         Parameters
         ----------
-        parameter_values : list of `nterms` scalars or ndarrays
+        parameters : list of `nterms` scalars or ndarrays
             Parameter values at which the operators matrices are known.
         matrices : list of `nterms` ndarray, all of the same shape.
-            Operator entries corresponding to the `parameter_values`.
+            Operator entries corresponding to the `parameters`.
+        InterpolatorClass : type
+            Class for the elementwise interpolation. Must obey the syntax
+            >>> interpolator = InterpolatorClass(data_points, data_values)
+            >>> interpolator_evaluation = interpolator(new_data_point)
+            This is usually a class from scipy.interpolate.
         kwargs
             Additional arguments for the interpolator.
         """
         _BaseParametricOperator.__init__(self)
 
         # Ensure there are the same number of parameter samples and matrices.
-        n_params, n_matrices = len(parameter_values), len(matrices)
+        n_params, n_matrices = len(parameters), len(matrices)
         if n_params != n_matrices:
-            raise ValueError(f"{n_params} = len(parameter_values) "
+            raise ValueError(f"{n_params} = len(parameters) "
                              f"!= len(matrices) = {n_matrices}")
 
+        # Preprocess matrices (e.g., nan/inf checking, compression as needed)
+        matrices = [self.OperatorClass(A).entries for A in matrices]
+
         # Ensure parameter / matrix shapes are consistent.
-        self._check_shape_consistency(parameter_values, "parameter sample")
+        self._check_shape_consistency(parameters, "parameter sample")
         self._check_shape_consistency(matrices, "operator matrix")
 
         # Construct the spline.
-        self.__parameter_values = parameter_values
+        self.__parameters = parameters
         self.__matrices = matrices
-        self.__interpolator = self.InterpolatorClass(parameter_values,
-                                                     matrices, **kwargs)
+        self.__interpolator = InterpolatorClass(self.parameters,
+                                                self.matrices, **kwargs)
 
     @property
-    def parameter_values(self):                            # pragma: no cover
+    def parameters(self):                            # pragma: no cover
         """Parameter values at which the operators matrices are known."""
-        return self.__parameter_values
+        return self.__parameters
 
     @property
     def matrices(self):
-        """Operator matrices corresponding to the parameter_values."""
+        """Operator matrices corresponding to the parameters."""
         return self.__matrices
 
     @property
@@ -124,92 +126,121 @@ class _InterpolatedOperator(_BaseParametricOperator):
             return False
         if self.shape != other.shape:
             return False
-        paramshape = np.shape(self.parameter_values[0])
-        if paramshape != np.shape(other.parameter_values[1]):
+        paramshape = np.shape(self.parameters[0])
+        if paramshape != np.shape(other.parameters[1]):
             return False
         if any(not np.all(left == right)
-               for left, right in zip(self.parameter_values,
-                                      other.parameter_values)):
+               for left, right in zip(self.parameters,
+                                      other.parameters)):
             return False
         return all(np.all(left == right)
                    for left, right in zip(self.matrices, other.matrices))
 
 
-# One-dimensional cubic spline operators ======================================
-class Spline1dConstantOperator(_InterpolatedOperator):
-    """Constant operator with 1D Spline interpolation, i.e.,
+# Public interpolated operator classes ========================================
+class InterpolatedConstantOperator(_InterpolatedOperator):
+    """Constant operator with elementwise interpolation, i.e.,
 
-        c(µ) = cubic_spline([µ1, µ2, ...], [c1[i,j], c2[i,j], ...])(µ),
+        c(µ) = Interpolator([µ1, µ2, ...], [c1[i,j], c2[i,j], ...])(µ),
 
     where c1 is the operator vector corresponding to the parameter µ1, etc.
     The vector c(µ) for a given µ is constructed by calling the object.
 
     Attributes
     ----------
-    parameter_values : list of `nterms` scalars or ndarrays
-        Parameter values at which the operators vectors are known.
+    parameters : list of `nterms` scalars or ndarrays
+        Parameter values at which the operators matrices are known.
     matrices : list of `nterms` ndarray, all of the same shape.
-        Operator vectors corresponding to the `parameter_values`.
+        Operator matrices corresponding to the `parameters`, i.e.,
+        `matrices[i]` are the operator entries at the value `parameters[i]`.
+    shape : tuple
+        Shape of the operator entries.
+    OperatorClass : class
+        Class of operator to construct, a subclass of
+        core.operators._BaseNonparametricOperator.
+    interpolator : scipy.interpolate class (or similar)
+        Object that constructs the operator at new parameter values, i.e.,
+        >>> new_operator_entries = interpolator(new_parameter)
     """
     _OperatorClass = ConstantOperator
-    _InterpolatorClass = scipy.interpolate.CubicSpline
 
 
-class Spline1dLinearOperator(_InterpolatedOperator):
-    """Linear operator with elementwise 1D cubic spline interpolation, i.e.,
+class InterpolatedLinearOperator(_InterpolatedOperator):
+    """Linear operator with elementwise interpolation, i.e.,
 
-        A(µ) = cubic_spline([µ1, µ2, ...], [A1[i,j], A2[i,j], ...])(µ),
+        A(µ) = Interpolator([µ1, µ2, ...], [A1[i,j], A2[i,j], ...])(µ),
 
     where A1 is the operator matrix corresponding to the parameter µ1, etc.
     The matrix A(µ) for a given µ is constructed by calling the object.
 
     Attributes
     ----------
-    parameter_values : list of `nterms` scalars or ndarrays
+    parameters : list of `nterms` scalars or ndarrays
         Parameter values at which the operators matrices are known.
     matrices : list of `nterms` ndarray, all of the same shape.
-        Operator matrices corresponding to the `parameter_values`.
+        Operator matrices corresponding to the `parameters`, i.e.,
+        `matrices[i]` are the operator entries at the value `parameters[i]`.
+    shape : tuple
+        Shape of the operator entries.
+    OperatorClass : class
+        Class of operator to construct, a subclass of
+        core.operators._BaseNonparametricOperator.
+    interpolator : scipy.interpolate class (or similar)
+        Object that constructs the operator at new parameter values, i.e.,
+        >>> new_operator_entries = interpolator(new_parameter)
     """
     _OperatorClass = LinearOperator
-    _InterpolatorClass = scipy.interpolate.CubicSpline
 
 
-class Spline1dQuadraticOperator(_InterpolatedOperator):
-    """Quadratic operator with elementwise 1D cubic spline interpolation, i.e.,
+class InterpolatedQuadraticOperator(_InterpolatedOperator):
+    """Quadratic operator with elementwise interpolation, i.e.,
 
-        H(µ) = cubic_spline([µ1, µ2, ...], [H1[i,j], H2[i,j], ...])(µ),
+        H(µ) = Interpolator([µ1, µ2, ...], [H1[i,j], H2[i,j], ...])(µ),
 
     where H1 is the operator matrix corresponding to the parameter µ1, etc.
     The matrix H(µ) for a given µ is constructed by calling the object.
 
     Attributes
     ----------
-    parameter_values : list of `nterms` scalars or ndarrays
+    parameters : list of `nterms` scalars or ndarrays
         Parameter values at which the operators matrices are known.
     matrices : list of `nterms` ndarray, all of the same shape.
-        Operator matrices corresponding to the `parameter_values`.
+        Operator matrices corresponding to the `parameters`, i.e.,
+        `matrices[i]` are the operator entries at the value `parameters[i]`.
+    shape : tuple
+        Shape of the operator entries.
+    OperatorClass : class
+        Class of operator to construct, a subclass of
+        core.operators._BaseNonparametricOperator.
+    interpolator : scipy.interpolate class (or similar)
+        Object that constructs the operator at new parameter values, i.e.,
+        >>> new_operator_entries = interpolator(new_parameter)
     """
     _OperatorClass = QuadraticOperator
-    _InterpolatorClass = scipy.interpolate.CubicSpline
 
 
-class Spline1dCubicOperator(_InterpolatedOperator):
-    """Cubic operator with elementwise 1D cubic spline interpolation, i.e.,
+class InterpolatedCubicOperator(_InterpolatedOperator):
+    """Cubic operator with elementwise interpolation, i.e.,
 
-        G(µ) = cubic_spline([µ1, µ2, ...], [G1[i,j], G2[i,j], ...])(µ),
+        G(µ) = Interpolator([µ1, µ2, ...], [G1[i,j], G2[i,j], ...])(µ),
 
     where G1 is the operator matrix corresponding to the parameter µ1, etc.
     The matrix G(µ) for a given µ is constructed by calling the object.
 
     Attributes
     ----------
-    parameter_values : list of `nterms` scalars or ndarrays
+    parameters : list of `nterms` scalars or ndarrays
         Parameter values at which the operators matrices are known.
     matrices : list of `nterms` ndarray, all of the same shape.
-        Operator matrices corresponding to the `parameter_values`.
+        Operator matrices corresponding to the `parameters`, i.e.,
+        `matrices[i]` are the operator entries at the value `parameters[i]`.
+    shape : tuple
+        Shape of the operator entries.
+    OperatorClass : class
+        Class of operator to construct, a subclass of
+        core.operators._BaseNonparametricOperator.
+    interpolator : scipy.interpolate class (or similar)
+        Object that constructs the operator at new parameter values, i.e.,
+        >>> new_operator_entries = interpolator(new_parameter)
     """
     _OperatorClass = CubicOperator
-    _InterpolatorClass = scipy.interpolate.CubicSpline
-
-
-# N-dimensional linear interpolation operators ================================
