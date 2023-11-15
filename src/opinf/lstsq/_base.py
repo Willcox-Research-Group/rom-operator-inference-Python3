35f36335e1837ade064460531f893deb54fb47f5
mtezzele <marcotez@gmail.com>
2023-11-15 15:29:39 -0600

    total least square solver

--- src/opinf/lstsq/_base.py
+++ src/opinf/lstsq/_base.py
@@ -4,6 +4,7 @@
 __all__ = [
     "lstsq_size",
     "PlainSolver",
+    "TotalLeastSquaresSolver"
 ]
 
 import abc
@@ -223,3 +224,26 @@ class PlainSolver(_BaseSolver):
     def predict(self):
         """Solve the least-squares problem."""
         return la.lstsq(self.A, self.B, **self.options)[0]
+
+
+class TotalLeastSquaresSolver(_BaseSolver):
+    """Solve the total least-squares problem without any
+    regularization, i.e.,
+
+        argmin_{X, G, H} ||[G H]||_F such that (A+G)X = B+H
+
+    The solution is calculated using standard computations
+    (see Wikipedia for example).
+    """
+    _LSTSQ_LABEL = r"argmin_{X, G, H} ||[G H]||_F | (A+G)X = B+H"
+
+    def predict(self):
+        """Solve the total least-squares problem."""
+        # A augmented with B
+        Vh = la.svd(np.hstack((self.A, self.B)), full_matrices=False)[2]
+        n = self.A.shape[1]
+        d = self.B.shape[1]
+        V = Vh.T
+        # This line could throw a la.LinAlgError exception
+        inv_VBB = la.inv(V[n:, -d:])
+        return -V[:n, n:] @ inv_VBB
