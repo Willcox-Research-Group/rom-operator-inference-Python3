72e12e8cfc1a8778963ad93058958562e9954ab8
Shane <smcquar@sandia.gov>
2024-03-28 12:34:15 -0600

    literature formatting and consistency

--- docs/bib2md.py
+++ docs/bib2md.py
@@ -1,43 +1,68 @@
 # bib2md.py
 """Generate the markdown literature page from bibtex."""
 
-# import collections
+import re
+import collections
 
 import bibtexparser
 
 
 HEADER = """# Literature
 
-This page lists scholar publications that develop, extend, or use Operator
-Inference.
+This page lists scholarly publications that develop, extend, or apply
+Operator Inference.
 """
-
-# For new publications, [click here submit a new entry](TODO).
+# [Click here submit a new entry](TODO).
 
 
 # Google Scholar IDs (https://scholar.google.com/citation?user=<this ID>)
 scholarIDS = {
+    "abidnazari": "u8vJ9-oAAAAJ",
+    "ashley": "9KFAXLYAAAAJ",
     "benner": "6zcRrC4AAAAJ",
+    "chaudhuri": "oGL9YJIAAAAJ",
     "duff": "OAkPFdkAAAAJ",
     "farcas": "Cts5ePIAAAAJ",
     "geelen": "vBzKRMsAAAAJ",
     "ghattas": "A5vhsIYAAAAJ",
+    "gomes": "s6mocWAAAAAJ",
     "goyal": "9rEfaRwAAAAJ",
+    "gruber": "CJVuqfoAAAAJ",
     "guo": "eON6MykAAAAJ",
     "hartmann": "4XvBneEAAAAJ",
+    "heiland": "wkHSeoYAAAAJ",
     "huang": "lUXijaQAAAAJ",
     "issan": "eEIe19oAAAAJ",
+    "ju": "JkKUWoAAAAAJ",
+    "junior": "66DEy5wAAAAJ",
+    "karasözen": "R906kj0AAAAJ",
     "khodabakhshi": "lYr_g-MAAAAJ",
+    "koike": "HFoIGcMAAAAJ",
     "kramer": "yfmbPNoAAAAJ",
-    "mcQuarrie": "qQ6JDJ4AAAAJ",
+    "mcquarrie": "qQ6JDJ4AAAAJ",
+    "najera-flores": "HJ-Dfl8AAAAJ",
     "peherstorfer": "C81WhlkAAAAJ",
     "qian": "jnHI7wQAAAAJ",
     "sharma": "Pb-tL5oAAAAJ",
+    "swischuk": "L9D0LBsAAAAJ",
     "tezaur": "Q3fx78kAAAAJ",
+    "todd": "jzY8TSkAAAAJ",
+    "tolley": "0kOHVOkAAAAJ",
     "uy": "hNN_KRQAAAAJ",
+    "wen": "uXJoQCAAAAAJ",
     "willcox": "axvGyXoAAAAJ",
     "wright": "VFQRIOwAAAAJ",
+    "yıldız": "UVPD79MAAAAJ",
 }
+
+
+specialChars = (
+    (r"\~{a}", "ã"),
+    (r"\'{e}", "é"),
+    (r"\i{}", "ı"),
+    (r"\"{o}", "ö"),
+    (r"\"{u}", "ü"),
+)
 
 
 # Fields in the BibTex file to ignore.
@@ -49,17 +74,34 @@ toSkip = {
 
 # Categories to group the references by.
 categories = {
-    "method": "Operator Inference methodologies",
-    "theory": "Operator Inference theory",
-    "struct": "Operator Inference with structure",
-    "application": "Applications",
+    "origin": "Original Paper",
     "survey": "Surveys",
+    "method": "Operator Inference Methodologies",
+    "theory": "Operator Inference Theory",
+    "struct": "Operator Inference with Structure",
+    "application": "Applications",
     "other": "Other",
 }
 
-# define string for each category
-categorieswww = {key: "" for key in categories}
-# categorieswww = collections.defaultdict()
+
+def clean_name(name):
+    r"""Handle special LaTex characters in names:
+    \~{a} -> ã
+    \'{e} -> é
+    \i{} -> ı
+    \"{o} -> ö
+    \"{u} -> ü
+    """
+    for before, after in specialChars:
+        name = name.replace(before, after)
+    return name
+
+
+def clean_title(title):
+    r"""Remove extra braces, etc. from paper titles,
+    e.g.,"{B}\'{e}nard" -> Bénard.
+    """
+    return re.subn(r"\{(\w+?)\}", r"\1", clean_name(title))[0]
 
 
 def linkedname(firstname, lastname):
@@ -78,28 +120,24 @@ def entry2txt(bibraw):
     """Convert the bibtexparser entry to a printable string."""
     txt = f"@{bibraw.entry_type}{{{bibraw.key},\n"
     for field in bibraw.fields:
-        key = field.key
-        if key in toSkip:
+        if (key := field.key) in toSkip:
             continue
-        txt = f"{txt}      {key} = {{{bibraw[key]}}},\n"
-    return f"{txt}  }}"
+        txt = f"{txt}&nbsp;&nbsp;{key} = {{{bibraw[key]}}},\n"
+    txt = f"{txt}  }}".replace("\\", "\\\\")
+    return txt.replace('\\\\"', '\\\\\\\\"')
 
 
 def main(bibfile, mdfile):
     """Convert a BibTex file to Markdown."""
 
     library = bibtexparser.parse_file(bibfile)
+    sectiontxt = collections.defaultdict(list)
 
     for entry in sorted(
         library.entries,
         key=lambda x: x["year"],
         reverse=False,
     ):
-        # Parse the category.
-        cat = "other"
-        if "category" in entry:
-            cat = entry["category"]
-
         # Parse authors.
         authors = []
         for author in entry["author"].split(" and "):
@@ -107,46 +145,45 @@ def main(bibfile, mdfile):
             if "," in author:
                 raise ValueError(f"change {bibfile} to avoid ',' in authors")
             names = author.split(" ")
-            initials = " ".join([name[0] + "." for name in names])
-            authors.append(linkedname(initials, names[-1]))
+            initials = " ".join([name[0] + "." for name in names[:-1]])
+            authors.append(linkedname(initials, clean_name(names[-1])))
 
         if len(authors) == 0:
             raise ValueError("empty author field")
         if len(authors) == 1:
-            line1 = f"- {authors[0]}"
+            authortxt = f"* {authors[0]}"
         elif len(authors) == 2:
-            line1 = f"- {authors[0]} and {authors[1]}"
+            authortxt = f"* {authors[0]} and {authors[1]}"
         else:
-            line1 = "- " + ", ".join(authors[:-1]) + f", and {authors[-1]}"
+            authortxt = "* " + ", ".join(authors[:-1]) + f", and {authors[-1]}"
 
         # Parse paper title.
+        title = clean_title(entry["title"])
         if "url" in entry:
-            line2 = f"[**{entry['title']}**]({entry['url']})"
+            titletxt = f"[**{title}**]({entry['url']})"
         else:
-            line2 = f"**{entry['title']}**"
+            titletxt = f"**{title}**"
 
         # Parse journal and year.
-        line3 = (
+        citetxt = (
             f"{entry['journal']}, {entry['year']} "
-            "<details><summary>BibTeX</summary><pre>"
+            f"<details><summary>BibTeX</summary><pre>{entry2txt(entry)}"
+            f"</pre></details>"
         )
-
-        line4 = f"{entry2txt(entry)}</pre></details>"
 
         # Combine parsed data.
-        categorieswww[cat] = (
-            categorieswww[cat]
-            + "\n"
-            + "  \n  ".join([line1, line2, line3, line4])
-        )
+        cat = "other"
+        if "category" in entry:
+            cat = entry["category"]
+        sectiontxt[cat].append("  \n  ".join([authortxt, titletxt, citetxt]))
 
     with open(mdfile, "w") as outfile:
         outfile.write(HEADER)
-        for cat in categorieswww:
-            if categorieswww[cat] == "":
+        for cat in categories:
+            if cat not in sectiontxt:
                 continue
-            outfile.write("\n## " + categories[cat] + "\n")
-            outfile.write(categorieswww[cat] + "\n")
+            outfile.write(f"\n## {categories[cat]}\n")
+            outfile.write("\n  <p></p>\n".join(sectiontxt[cat]) + "\n")
 
 
 if __name__ == "__main__":
