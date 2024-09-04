9f94e31b4d80e2a9209b49f800e4263860777df7
Shane <smcquar@sandia.gov>
2024-09-04 16:03:03 -0600

    tests and fixes for decode, project

--- tests/roms/test_base.py
+++ tests/roms/test_base.py
@@ -68,17 +68,23 @@ class _TestBaseROM(abc.ABC):
                 "basis not derived from BasisTemplate or BasisMulti, "
                 "unexpected behavior may occur"
             )
-            assert wn[3].message.args[0] == (
-                "ddt_estimator not derived from DerivativeEstimatorTemplate, "
-                "unexpected behavior may occur"
-            )
+            if modutils.is_continuous(model):
+                assert wn[3].message.args[0] == (
+                    "ddt_estimator not derived from "
+                    "DerivativeEstimatorTemplate, "
+                    "unexpected behavior may occur"
+                )
+            else:
+                assert wn[3].message.args[0] == (
+                    "ddt_estimator ignored for discrete models"
+                )
 
             # Given ddt_estimator with non-continuous model.
             if modutils.is_discrete(model):
                 with pytest.warns(opinf.errors.OpInfWarning) as wn:
                     rom = self.ROM(
                         model,
-                        ddt_estimator=self._get_args("ddt_estimator"),
+                        ddt_estimator=self._get("ddt_estimator"),
                     )
                 assert len(wn) == 1
                 assert wn[0].message.args[0] == (
@@ -88,16 +94,17 @@ class _TestBaseROM(abc.ABC):
                 assert not rom._iscontinuous
 
             # Correct usage.
-            lifter, ddt_estimator = self._get("lifter", "ddt_estimator")
-            rom = self.ROM(
-                model,
-                lifter=lifter,
-                ddt_estimator=ddt_estimator,
-            )
-            assert rom.lifter is lifter
-            assert rom.transformer is None
-            assert rom.basis is None
-            assert rom.ddt_estimator is ddt_estimator
+            if modutils.is_continuous(model):
+                lifter, ddt_estimator = self._get("lifter", "ddt_estimator")
+                rom = self.ROM(
+                    model,
+                    lifter=lifter,
+                    ddt_estimator=ddt_estimator,
+                )
+                assert rom.lifter is lifter
+                assert rom.transformer is None
+                assert rom.basis is None
+                assert rom.ddt_estimator is ddt_estimator
 
             transformer, basis = self._get("multi_transformer", "multi_basis")
             rom = self.ROM(
@@ -187,18 +194,111 @@ class _TestBaseROM(abc.ABC):
             for out in out1, out2:
                 _check(out, (r,))
 
-    def test_decode(self):
+    def test_decode(self, n=22, k=18):
         """Test decode()."""
-        raise NotImplementedError
 
-    def test_project(self):
+        def _check(arr, shape):
+            assert isinstance(arr, np.ndarray)
+            assert arr.shape == shape
+
+        for model in self._get_models():
+            # Lifter only.
+            rom = self.ROM(model, lifter=self._get("lifter"))
+            states = np.random.random((2 * n, k))
+            _check(rom.decode(states), (n, k))
+            _check(rom.decode(states[:, 0]), (n,))
+
+            # Transformer only.
+            rom = self.ROM(model, transformer=self._get("transformer"))
+            with pytest.raises(AttributeError) as ex:
+                rom.decode(states)
+            assert ex.value.args[0] == (
+                "transformer not trained (call fit() or fit_transform())"
+            )
+            states = np.random.random((n, k))
+            states_ = rom.encode(states, fit_transformer=True)
+            out = rom.decode(states_)
+            _check(out, (n, k))
+            assert np.allclose(out, states)
+            out = rom.decode(states_[:, 0])
+            _check(out, (n,))
+            assert np.allclose(out, states[:, 0])
+
+            # Basis only.
+            rom = self.ROM(model, basis=self._get("basis"))
+            with pytest.raises(AttributeError) as ex:
+                rom.decode(states)
+            assert ex.value.args[0] == "basis entries not initialized"
+            states_ = rom.encode(states, fit_basis=True)
+            _check(rom.decode(states_), (n, k))
+            _check(rom.decode(states_[:, 0]), (n,))
+
+            # Lifter, transformer, and basis.
+            a1, a2, a3 = self._get("lifter", "transformer", "basis")
+            rom = self.ROM(model, lifter=a1, transformer=a2, basis=a3)
+            states_ = rom.encode(states, fit_transformer=True, fit_basis=True)
+            out1 = rom.decode(states_)
+            _check(out1, (n, k))
+            out2 = rom.decode(states_[:, 0])
+            _check(out2, (n,))
+            assert np.allclose(out2, out1[:, 0])
+
+            # With the locs argument.
+            a2, a3 = self._get("transformer", "basis")
+            rom = self.ROM(model, transformer=a2, basis=a3)
+            states_ = rom.encode(states, fit_transformer=True, fit_basis=True)
+            out1 = rom.decode(states_)
+            locs = np.sort(np.random.choice(n, n // 3))
+            out2 = rom.decode(states_, locs=locs)
+            _check(out2, (n // 3, k))
+            assert np.allclose(out2, out1[locs])
+
+    def test_project(self, n=30, k=19):
         """Test project()."""
-        raise NotImplementedError
+        states = np.random.random((n, k))
 
+        def _check(rom, preserved=False):
+            rom.encode(states, fit_transformer=True, fit_basis=True)
+            out = rom.project(states)
+            assert isinstance(out, np.ndarray)
+            assert out.shape == (n, k)
+            if preserved:
+                assert np.allclose(out, states)
+            out0 = rom.project(states[:, 0])
+            assert isinstance(out0, np.ndarray)
+            assert out0.shape == (n,)
+            assert np.allclose(out0, out[:, 0])
+
+        for model in self._get_models():
+            # Lifter only.
+            _check(self.ROM(model, lifter=self._get("lifter")), preserved=True)
+
+            # Transformer only.
+            rom = self.ROM(model, transformer=self._get("transformer"))
+            with pytest.raises(AttributeError) as ex:
+                rom.project(states)
+            assert ex.value.args[0] == (
+                "transformer not trained (call fit() or fit_transform())"
+            )
+            _check(rom, preserved=True)
+
+            # Basis only.
+            rom = self.ROM(model, basis=self._get("basis"))
+            with pytest.raises(AttributeError) as ex:
+                rom.project(states)
+            assert ex.value.args[0] == "basis entries not initialized"
+            _check(rom, preserved=False)
+
+            # Lifter, transformer, and basis.
+            a1, a2, a3 = self._get("lifter", "transformer", "basis")
+            _check(self.ROM(model, lifter=a1, transformer=a2, basis=a3))
+
+    @abc.abstractmethod
     def test_fit(self):
         """Test fit()."""
-        raise NotImplementedError
+        raise NotImplementedError  # pragma: no cover
 
+    @abc.abstractmethod
     def test_predict(self):
         """Test predict()."""
-        raise NotImplementedError
+        raise NotImplementedError  # pragma: no cover
